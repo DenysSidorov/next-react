c9fe7fdbdb9dc649b513f7c43ea1a31c9d5ae42a
Denys Sidorov <000scorpions000@gmail.com>
2020-11-25 07:08:37 +0200

    add metas on page

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,19 @@
+import React from 'react';
+import Head from 'next/head';
+
 export default function About() {
-  return <div>About page</div>
+  return (
+    <React.Fragment>
+      <Head>
+        <meta charSet="UTF-8"/>
+        <title>About page</title>
+        <meta name="description" content="This is about page"/>
+        <meta name="keywords" content="HTML, CSS, JavaScript"/>
+        <meta name="author" content="John Doe"/>
+      </Head>
+
+      <div>About page</div>
+
+    </React.Fragment>
+  )
 }
