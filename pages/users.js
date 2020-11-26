2220b0895994904a9bda0b0b803caa270703b339
Denys Sidorov <000scorpions000@gmail.com>
2020-11-26 08:25:14 +0200

    get prefetched data from server before front's render (SEO)

--- pages/users.js
+++ pages/users.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
-import Router from 'next/router'
+import Router from 'next/router';
 import Header from '../components/Header';
-export default function Users() {
-console.log('Router', Router);
+
+export default function Users({users}) {
+  console.log('==== users ==== ', users);
+  console.log('Router', Router);
 
   function clickNavHandler() {
     console.log('clickNavHandler');
@@ -14,8 +16,34 @@ console.log('Router', Router);
     <Header>
       <div>
         Users page
+        <ul>
+          {users.map((el) => {
+            return (
+              <li key={el.id}>
+                <Link href={`/user/${el.id}`}>
+                  <a>
+                    {el.name} with id - {el.id}
+                  </a>
+                </Link>
+              </li>
+            );
+          }
+          )}
+        </ul>
       </div>
-      <div onClick={clickNavHandler}>Program navigation, Click Me</div>
+      <button onClick={clickNavHandler}>Program navigation, Click Me</button>
     </Header>
-   )
+  );
 }
+
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:4000/users');
+  const users = await res.json();
+  return {
+    props: {
+      users
+    }
+  };
+}
+
